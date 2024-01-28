5fe1625b603588259c5e6f1a56abe122250f9c26
VIPon4ik <umniyd@gmail.com>
2024-01-28 16:31:42 +0200

    Добавьте файлы проекта.

---
+++ Laba1_Perepelytsia/Laba1_Perepelytsia.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	srand(time(0));
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int length = 4;
+	string password = "";
+
+	cout << "Введіть довжину пароля (4-32): ";
+	cin >> length;
+
+	if (length > 32) {
+		length = 32;
+	}
+	else if (length < 4) {
+		length = 4;
+	}
+
+
+	for (int j = 0; j < 10; j++) {
+
+		for (int i = 0; i < length; i++) {
+			int randomNum = rand() % (126 - 33 + 1) + 33;
+			password += char(randomNum);
+		}
+
+		cout << password << endl;
+		password = "";
+	}
+}
