3432a43429b5c3953b9dffbdb87fee9ddf386c11
VIPon4ik <umniyd@gmail.com>
2024-01-28 18:09:35 +0200

    Done task, but not a very good solution

--- Laba1_Perepelytsia/Laba1_Perepelytsia.cpp
+++ Laba1_Perepelytsia/Laba1_Perepelytsia.cpp
@@ -6,14 +6,35 @@
 
 using namespace std;
 
+char getRandomChar(bool withDigits, bool withUpperChars, bool withSymbols) {
+	int rangeSelector = rand() % 4;
+
+	switch (rangeSelector) {
+	case 0:
+		if (withDigits) {
+			return char(rand() % 10 + 48);
+		}
+	case 1:
+		if (withUpperChars) {
+			return char(rand() % 26 + 65);
+		}
+	case 2:
+		if (withSymbols) {
+			return char(rand() % 15 + 33);
+		}
+	default:
+		return char(rand() % 26 + 97);
+	}
+}
+
 int main()
 {
-	srand(time(0));
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	int length = 4;
-	string password = "";
+	int length;
+	bool withDigits = false, withUpperChars = false, withSymbols = false;
+	string password = "", digitsSetting, upperCharsSetting, symbolsSetting;
 
 	cout << "Введіть довжину пароля (4-32): ";
 	cin >> length;
@@ -25,12 +46,30 @@ int main()
 		length = 4;
 	}
 
+	cout << "Чи буде містити цифри (yes/no): ";
+	cin >> digitsSetting;
+	cout << "Чи буде містити заглавні букви (yes/no): ";
+	cin >> upperCharsSetting;
+	cout << "Чи буде містити символи (yes/no): ";
+	cin >> symbolsSetting;
+
+	if (digitsSetting == "yes") {
+		withDigits = true;
+	}
+
+	if (upperCharsSetting == "yes") {
+		withUpperChars = true;
+	}
+
+	if (symbolsSetting == "yes") {
+		withSymbols = true;
+	}
 
 	for (int j = 0; j < 10; j++) {
 
 		for (int i = 0; i < length; i++) {
-			int randomNum = rand() % (126 - 33 + 1) + 33;
-			password += char(randomNum);
+			password += getRandomChar(withDigits, withUpperChars, withSymbols);
+			
 		}
 
 		cout << password << endl;
